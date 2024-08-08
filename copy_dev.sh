681af2ffadc3bd64d3f882d960d59e5de2265be5
Raul Castro Palmett <rcastrop@outlook.com>
2024-08-08 10:07:31 -0500

    Ajustes versio

--- copy_dev.sh
+++ copy_dev.sh
@@ -1,1 +1,2 @@
 cp -r *_handler /home/rcastro/Documentos/temporal/mindsdb/mindsdb/integrations/handlers
+cp ml_exec_base.py /home/rcastro/Documentos/temporal/mindsdb/mindsdb/integrations/libs/ml_exec_base.py

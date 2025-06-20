cbcce1b040a036c8e6739a52257f4a4bd5e2e5f6
Raul Castro Palmett <rcastrop@outlook.com>
2025-06-20 16:05:54 -0500

    Ajuste de instalacion

--- copy_dev.sh
+++ copy_dev.sh
@@ -1,2 +1,6 @@
 cp -r *_handler /home/rcastro/Documentos/temporal/mindsdb/mindsdb/integrations/handlers
 cp ml_exec_base.py /home/rcastro/Documentos/temporal/mindsdb/mindsdb/integrations/libs/ml_exec_base.py
+
+
+# dentro del contenedor docker
+cp -r *_handler /mindsdb/mindsdb/integrations/handlers

c6dee416f5c2106c184b50acd4c0ede284475c87
Raul Castro Palmett <rcastrop@outlook.com>
2025-06-20 16:23:17 -0500

    Actualizacion instalacion handler

--- copy_dev.sh
+++ copy_dev.sh
@@ -4,5 +4,5 @@ cp ml_exec_base.py /home/rcastro/Documentos/temporal/mindsdb/mindsdb/integration
 
 # dentro del contenedor docker
 cp -r *_handler /mindsdb/mindsdb/integrations/handlers
-
-pip install mindsdb[s3_ngx_handler]
+ 
+pip install .[s3ngx_handler]
